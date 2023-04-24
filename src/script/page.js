d39c6154fa3470250cb035b00f7c98d8acc49bdd
dksubconscious <theone@pocketninjadesign.com>
2023-04-24 00:18:31 -0700

    Added GSAP test

--- src/script/page.js
+++ src/script/page.js
@@ -19,3 +19,17 @@ window.addEventListener('resize', debounce(pageHeightSetter, 50));
 
 // run it first
 pageHeightSetter();
+
+
+
+// Quick gsap test
+const t1 = new TimelineLite();
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('mouseenter', () => {
+  t1.to( logo, 0.5, { scale: 1.2,  ease: Elastic.easeOut.config(1, 0.3) } );
+});
+
+logo.addEventListener('mouseleave', () => {
+  t1.to( logo, 0.25, { scale: 1,  ease: Elastic.easeOut.config(1, 0.3) } );
+});
